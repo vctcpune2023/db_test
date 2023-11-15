20219939613d7b28b115698726ab154f961f9a93
shridharshende <shridharshende@vctcpune.com>
2023-11-15 15:15:33 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from  flask import Flask,request,jsonify,render_template
+import config
+from Model.utils import MedicalInsurance
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/predicted_charges",methods = ["POST"])
+def get_medicalInsurance():
+    
+    data = request.form 
+    age = eval(data["age"])
+    gender = data["gender"]
+    bmi = eval(data["bmi"])
+    children = int(data["children"])
+    smoker = data["smoker"]
+    region = data["region"]
+
+    medical = MedicalInsurance(age,gender,bmi,smoker,children,region)
+    charges = medical.get_predicted_charges()
+    data_dict = dict(data)
+    data_dict["result"] = charges
+    return render_template("result.html",charges=data_dict)
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=9696)
---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+model_path=os.path.join("Model","KNN_model.pkl")
+scaler_path=os.path.join("Model","scale.pkl")
+json_path=os.path.join("Model","project_data.json")
